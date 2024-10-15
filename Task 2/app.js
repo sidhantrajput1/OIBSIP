b16c6cf8ea7bb294f677727a665b3f04777bec51
sidhantrajput1 <sidhant808310@gmail.com>
2024-10-15 19:01:25 +0530

    Card Added

--- Task 2/app.js
+++ Task 2/app.js
@@ -1,18 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const toggle = document.querySelector('.toggle'); 
-    const navItems = document.querySelector('.nav-links');
+// document.addEventListener('DOMContentLoaded', () => {
+//     const toggle = document.querySelector('.toggle'); 
+//     const navItems = document.querySelector('.nav-links');
 
-    toggle.addEventListener('click', () => {
-        navItems.classList.toggle('active'); 
-    });
-});
+//     toggle.addEventListener('click', () => {
+//         navItems.classList.toggle('active'); 
+//     });
+// });
 
-document.getElementById('toggleCheckbox').addEventListener('change', function() {
-    if (this.checked) {
-        document.body.style.backgroundColor = 'white'; 
-        document.body.style.color = 'black';
-    } else {
-        document.body.style.backgroundColor = 'black'; 
-        document.body.style.color = 'white';
-    }
-});
+// document.getElementById('toggleCheckbox').addEventListener('change', function() {
+//     if (this.checked) {
+//         document.body.style.backgroundColor = 'white'; 
+//         document.body.style.color = 'black';
+//     } else {
+//         document.body.style.backgroundColor = 'black'; 
+//         document.body.style.color = 'white';
+//     }
+// });
